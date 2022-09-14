3dda4321d4f42b4bbb8d419b1e7a0e7b5c89c923
LeinadRPFI <55161486+LeinadRPFI@users.noreply.github.com>
2022-09-14 17:39:28 -0300

    Configurando htaccess
    Configuração do controller, para utilização de dados presentes na url.

---
+++ core/ConfigController.php
@@ -0,0 +1,33 @@
+<?php
+
+    namespace Core;
+
+    class ConfigController
+    {
+        private string $url;
+        private string $urlController;
+        private string $urlMetodo;
+        private array $urlArray;
+
+        public function __construct()
+        {
+            if(!empty(filter_input(INPUT_GET, 'url', FILTER_DEFAULT))){
+                $this->url = filter_input(INPUT_GET, 'url', FILTER_DEFAULT);
+                $this->urlArray = explode("/", $this->url);
+                //var_dump($this->urlArray);
+
+                if((isset($this->urlArray[0])) and (isset($this->urlArray[1]))){
+                    $this->urlController = $this->urlArray[0];
+                    $this->urlMetodo = $this->urlArray[1];
+                }else{
+                    $this->urlController = "home";
+                    $this->urlMetodo = "index";
+                }
+            }else{
+                $this->urlController = "home";
+                $this->urlMetodo = "index";
+            }
+
+            echo "Controller: {$this->urlController} - Método: {$this->urlMetodo} <br>";
+        }
+    }
