6be0ff6dda8aa87f2809225375cc0aa624609f0d
Brambs <gabriel128.br@gmail.com>
2022-11-16 15:06:21 -0300

    Update cadastroCurso.php

--- adm/app/adms/Views/cursos/cadastroCurso.php
+++ adm/app/adms/Views/cursos/cadastroCurso.php
@@ -7,10 +7,70 @@ if (isset($this->data['form'])) {
 
 if (isset($_SESSION['msg'])) {
     echo $_SESSION['msg'];
-    unset($_SESSION['msg']); 
+    unset($_SESSION['msg']);
 }
 ?>
 
+
+
+<main>
+    <div class="page-text">
+        <h2>DevGate</h2>
+        <p>Registrar um novo curso na plataforma</p>
+    </div>
+    <div class="gn-form">
+        <form action="">
+            <?php
+            $nomeCurso = "";
+            if (isset($valueForm['nomeCurso'])) {
+                $nomeCurso = $valueForm['nomeCurso'];
+            }
+            ?>
+            <input type="text" name="nomeCurso" id="name" value="<?php echo $nomeCurso ?>" required="required">
+
+            <?php
+            $descricao = "";
+            if (isset($valueForm['descricao'])) {
+                $descricao = $valueForm['descricao'];
+            }
+            ?>
+            <input type="descricao" name="descricao" id="descricao" value="<?php echo $descricao ?>" required="required">
+
+            <?php
+            $objetivos = "";
+            if (isset($valueForm['objetivos'])) {
+                $objetivos = $valueForm['objetivos'];
+            }
+            ?>
+            <input type="text" name="objetivos" value="<?php echo $objetivos ?>" required="required">
+
+            <?php
+            $hiperlink = "";
+            if (isset($valueForm['hiperlink'])) {
+                $hiperlink = $valueForm['hiperlink'];
+            }
+            ?>
+            <input type="text" name="hiperlink" value="<?php echo $hiperlink ?>" required="required">
+
+            <?php
+            $idResponsavel = "";
+            if (isset($valueForm['idResponsavel'])) {
+                $idResponsavel = $valueForm['idResponsavel'];
+            }
+            ?>
+            <input type="text" name="idResponsavel" value="<?php echo $idResponsavel ?>" required="required">
+
+
+            <!-- submit button -->
+            <input class="submit-btn" type="submit" value="Registrar Curso" />
+        </form>
+    </div>
+</main>
+
+
+
+
+<!-- Implementação antiga do Felipe -->
 <div class="main-cadastro">
     <div class="box-cadastro">
         <form method="POST" action="" class="form-cadastro">
