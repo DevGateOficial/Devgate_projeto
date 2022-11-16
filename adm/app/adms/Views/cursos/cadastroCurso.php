8d89ce9c22be34bd96672560f652c21325bd5315
Brambs <gabriel128.br@gmail.com>
2022-11-16 15:30:24 -0300

    update

--- adm/app/adms/Views/cursos/cadastroCurso.php
+++ adm/app/adms/Views/cursos/cadastroCurso.php
@@ -26,7 +26,7 @@ if (isset($_SESSION['msg'])) {
                 $nomeCurso = $valueForm['nomeCurso'];
             }
             ?>
-            <input type="text" name="nomeCurso" id="name" value="<?php echo $nomeCurso ?>" required="required">
+            <input type="text" placeholder="Nome do Curso" name="nomeCurso" id="name" value="<?php echo $nomeCurso ?>" required="required">
 
             <?php
             $descricao = "";
@@ -34,7 +34,7 @@ if (isset($_SESSION['msg'])) {
                 $descricao = $valueForm['descricao'];
             }
             ?>
-            <input type="descricao" name="descricao" id="descricao" value="<?php echo $descricao ?>" required="required">
+            <input type="descricao" placeholder="Descrição" name="descricao" id="descricao" value="<?php echo $descricao ?>" required="required">
 
             <?php
             $objetivos = "";
@@ -42,7 +42,7 @@ if (isset($_SESSION['msg'])) {
                 $objetivos = $valueForm['objetivos'];
             }
             ?>
-            <input type="text" name="objetivos" value="<?php echo $objetivos ?>" required="required">
+            <input type="text" placeholder="Objetivos" name="objetivos" value="<?php echo $objetivos ?>" required="required">
 
             <?php
             $hiperlink = "";
@@ -50,7 +50,7 @@ if (isset($_SESSION['msg'])) {
                 $hiperlink = $valueForm['hiperlink'];
             }
             ?>
-            <input type="text" name="hiperlink" value="<?php echo $hiperlink ?>" required="required">
+            <input type="text" placeholder="Link" name="hiperlink" value="<?php echo $hiperlink ?>" required="required">
 
             <?php
             $idResponsavel = "";
@@ -58,7 +58,7 @@ if (isset($_SESSION['msg'])) {
                 $idResponsavel = $valueForm['idResponsavel'];
             }
             ?>
-            <input type="text" name="idResponsavel" value="<?php echo $idResponsavel ?>" required="required">
+
 
 
             <!-- submit button -->
@@ -66,80 +66,3 @@ if (isset($_SESSION['msg'])) {
         </form>
     </div>
 </main>
-
-
-
-
-<!-- Implementação antiga do Felipe -->
-<div class="main-cadastro">
-    <div class="box-cadastro">
-        <form method="POST" action="" class="form-cadastro">
-            <h2>Sign up</h2>
-
-            <div class="inputBox-cadastro">
-                <?php
-                $nomeCurso = "";
-                if (isset($valueForm['nomeCurso'])) {
-                    $nomeCurso = $valueForm['nomeCurso'];
-                }
-                ?>
-                <input type="text" name="nomeCurso" id="name" value="<?php echo $nomeCurso ?>" required="required">
-                <span>Nome do curso</span>
-                <i></i>
-            </div>
-
-            <div class="inputBox-cadastro">
-                <?php
-                $descricao = "";
-                if (isset($valueForm['descricao'])) {
-                    $descricao = $valueForm['descricao'];
-                }
-                ?>
-                <input type="descricao" name="descricao" id="descricao" value="<?php echo $descricao ?>" required="required">
-                <span>Descrição</span>
-                <i></i>
-            </div>
-
-            <div class="inputBox-cadastro">
-                <?php
-                $objetivos = "";
-                if (isset($valueForm['objetivos'])) {
-                    $objetivos = $valueForm['objetivos'];
-                }
-                ?>
-                <input type="text" name="objetivos" value="<?php echo $objetivos ?>" required="required">
-                <span>Objetivos</span>
-                <i></i>
-            </div>
-
-            <div class="inputBox-cadastro">
-                <?php
-                $hiperlink = "";
-                if (isset($valueForm['hiperlink'])) {
-                    $hiperlink = $valueForm['hiperlink'];
-                }
-                ?>
-                <input type="text" name="hiperlink" value="<?php echo $hiperlink ?>" required="required">
-                <span>hiperlink</span>
-                <i></i>
-            </div>
-
-            <div class="inputBox-cadastro">
-                <?php
-                $idResponsavel = "";
-                if (isset($valueForm['idResponsavel'])) {
-                    $idResponsavel = $valueForm['idResponsavel'];
-                }
-                ?>
-                <input type="text" name="idResponsavel" value="<?php echo $idResponsavel ?>" required="required">
-                <span>Usuário responsável</span>
-                <i></i>
-            </div>
-
-            <div class="links-cadastro">
-                <input type="submit" name="Cadastrar" value="Cadastrar">
-                <!-- <a href="<?php echo URLADM; ?>login/index"> Sign in </a> -->
-            </div>
-        </form>
-    </div>
-</div>
--- adm/app/adms/Views/cursos/editCurso.php
+++ adm/app/adms/Views/cursos/editCurso.php
@@ -10,83 +10,69 @@ if (isset($_SESSION['msg'])) {
     unset($_SESSION['msg']);
 }
 ?>
-
-<div class="main-cadastro">
-    <div class="box-cadastro">
-        <form method="POST" action="" class="form-cadastro">
-            <h2>Edição de Curso</h2>
-
-            <?php
-            $idCurso = "";
-            if (isset($valueForm['idCurso'])) {
-                $idCurso = $valueForm['idCurso'];
-            }
-            ?>
-            <input type="hidden" name="idCurso" value="<?php echo $idCurso ?>">
-
-            <div class="inputBox-cadastro">
-                <?php
-                $nomeCurso = "";
-                if (isset($valueForm['nomeCurso'])) {
-                    $nomeCurso = $valueForm['nomeCurso'];
-                }
-                ?>
-                <input type="text" name="nomeCurso" id="name" value="<?php echo $nomeCurso ?>" required="required">
-                <span>Nome do curso</span>
-                <i></i>
-            </div>
-
-            <div class="inputBox-cadastro">
-                <?php
-                $descricao = "";
-                if (isset($valueForm['descricao'])) {
-                    $descricao = $valueForm['descricao'];
-                }
-                ?>
-                <input type="descricao" name="descricao" id="descricao" value="<?php echo $descricao ?>" required="required">
-                <span>Descrição</span>
-                <i></i>
-            </div>
-
-            <div class="inputBox-cadastro">
-                <?php
-                $objetivos = "";
-                if (isset($valueForm['objetivos'])) {
-                    $objetivos = $valueForm['objetivos'];
-                }
-                ?>
-                <input type="text" name="objetivos" value="<?php echo $objetivos ?>" required="required">
-                <span>Objetivos</span>
-                <i></i>
-            </div>
-
-            <div class="inputBox-cadastro">
-                <?php
-                $hiperlink = "";
-                if (isset($valueForm['hiperlink'])) {
-                    $hiperlink = $valueForm['hiperlink'];
-                }
-                ?>
-                <input type="text" name="hiperlink" value="<?php echo $hiperlink ?>" required="required">
-                <span>hiperlink</span>
-                <i></i>
-            </div>
-
-            <div class="inputBox-cadastro">
-                <?php
-                $idResponsavel = "";
-                if (isset($valueForm['idResponsavel'])) {
-                    $idResponsavel = $valueForm['idResponsavel'];
-                }
-                ?>
-                <input type="text" name="idResponsavel" value="<?php echo $idResponsavel ?>" required="required">
-                <span>Usuário responsável</span>
-                <i></i>
-            </div>
-
-            <div class="links-cadastro">
-                <input type="submit" name="EditCurso" value="Editar">
-            </div>
+<main>
+    <div class="page-text">
+        <h2>DevGate</h2>
+        <p>Registrar um novo curso na plataforma</p>
+    </div>
+    <div class="gn-form">
+        <form action="">
+            <input class="submit-btn" type="submit" value="Registrar Curso" />
         </form>
     </div>
-</div>
+</main>
+
+
+<main>
+    <div class="page-text">
+        <h2>Curso</h2>
+        <p>Pagina de Edição de curso</p>
+    </div>
+    <div class="gn-form">
+        <?php
+        $idCurso = "";
+        if (isset($valueForm['idCurso'])) {
+            $idCurso = $valueForm['idCurso'];
+        }
+        ?>
+        <input type="hidden" name="idCurso" value="<?php echo $idCurso ?>">
+
+        <?php
+        $nomeCurso = "";
+        if (isset($valueForm['nomeCurso'])) {
+            $nomeCurso = $valueForm['nomeCurso'];
+        }
+        ?>
+        <input type="text" name="nomeCurso" id="name" value="<?php echo $nomeCurso ?>" required="required">
+
+        <?php
+        $descricao = "";
+        if (isset($valueForm['descricao'])) {
+            $descricao = $valueForm['descricao'];
+        }
+        ?>
+        <input type="descricao" name="descricao" id="descricao" value="<?php echo $descricao ?>" required="required">
+
+        <?php
+        $objetivos = "";
+        if (isset($valueForm['objetivos'])) {
+            $objetivos = $valueForm['objetivos'];
+        }
+        ?>
+        <input type="text" name="objetivos" value="<?php echo $objetivos ?>" required="required">
+
+        <?php
+        $hiperlink = "";
+        if (isset($valueForm['hiperlink'])) {
+            $hiperlink = $valueForm['hiperlink'];
+        }
+        ?>
+        <input type="text" name="hiperlink" value="<?php echo $hiperlink ?>" required="required">
+
+
+        <input class="submit-btn" type="submit" value="Registrar Curso" />
+
+        </form>
+    </div>
+    </div>
+</main>
